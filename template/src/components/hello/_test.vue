34aed5c02c6fa75a382f47f889eead113af3299a
taoql <taoql@gold-finance.com.cn>
2017-05-15 18:23:15 +0800

    by taoqili init

---
+++ template/src/components/hello/_test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello-wrapper">
+    <jc-hello></jc-hello>
+  </div>
+</template>
+<script>
+  import hello from './index.vue'
+  export default {
+    name: 'test-hello',
+    components: {
+      'jc-hello': hello
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+<style lang="postcss" scoped>
+  .hello-wrapper {
+  }
+</style>

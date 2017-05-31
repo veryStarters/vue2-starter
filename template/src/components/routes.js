5ce0de54a6cae18dbd4a20c8fae18b3918a19013
taoql <taoql@gold-finance.com.cn>
2017-05-31 18:06:49 +0800

    by taoqili init

--- template/src/components/routes.js
+++
@@ -1,6 +0,0 @@
-/**
- * 新增路由请严格按照如下格式书写, 系统将自动在pages|components目录下生成对应模板文件
- * 变量名代表route的name，变量名请按照驼峰格式书写，每个驼峰单词将被切分成route的path  userLogin => /user/login
- * 所有components的路由自动增加/components前缀
- **/
-export const hello = r => require(['../components/hello/_test.vue'], r)

9ae84326ebff4fe4885bfa187841adb0248e13c3
taoql <taoql@gold-finance.com.cn>
2017-07-03 17:48:50 +0800

    by taoqili init

--- template/src/pages/common/layout/index.js
+++ template/src/pages/common/layout/index.js
@@ -3,7 +3,7 @@
  * @author taoqili
  * @date 2017/6/30
  */
-export const nav = require('./nav.vue')
+export const navbar = require('./navbar.vue')
 export const main = require('./main.vue')
 export const topbar = require('./topbar.vue')
 export const sidebar = require('./sidebar.vue')

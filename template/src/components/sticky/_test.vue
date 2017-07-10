30bbf266567c2494f55592a0c84e6d3f8f9b3225
taoql <taoql@gold-finance.com.cn>
2017-07-10 18:20:24 +0800

    by taoqili init

---
+++ template/src/components/sticky/_test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sticky-wrapper">
+    <jc-sticky></jc-sticky>
+  </div>
+</template>
+<script>
+  import sticky from './index.vue'
+  export default {
+    name: 'test-sticky',
+    components: {
+      'jc-sticky': sticky
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+<style lang="postcss" scoped>
+  .sticky-wrapper {
+  }
+</style>

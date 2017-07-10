da0d497858e306aa83691b317cd6ccafa5bc1c9a
taoql <taoql@gold-finance.com.cn>
2017-07-10 16:06:05 +0800

    by taoqili init

--- template/src/api-mock/user/info.js
+++ template/src/api-mock/user/info.js
@@ -1,21 +1,31 @@
+/**
+ * Created by Webstorm.
+ * 可以采用mockjs来自动生成mock数据
+ * http://mockjs.com/examples.html#DPD
+ * @author taoqili
+ * @date 2017/6/23
+ */
+var char = 'abcdefghijklmnopqrstuvwxyz';
+var Mock = require('mockjs')
+var userInfo = Mock.mock({
+  'name': '@cname',
+  'accessToken': '@string(char,32)',
+  'menus|3': [{
+    'label': '@string(char, 10)',
+    'path': '/@string(char,8)/@string(char,8)',
+    'children|3': [{
+      'label': '@string(char, 10)',
+      'path': '/@string(char,8)/@string(char,8)'
+    }]
+  }],
+  'roles|1-5': ['@string(char, 6)'],
+  'permissions|1-5': ['@string(char, 8)']
+})
 module.exports = function (req, res, next) {
   return {
     ret: 'success',
     code: 0,
     msg: '接口提示信息',
-    data: {
-      name: req.body.name || '客人',
-      accessToken: 'ajdksldjfkcirpqiwertjdljcv=djrkw',
-      menus: [],
-      roles: [
-        'admin',
-        'editor'
-      ],
-      permissions: [
-        '/',
-        '/test1',
-        '/test2'
-      ]
-    }
+    data: userInfo
   }
 }

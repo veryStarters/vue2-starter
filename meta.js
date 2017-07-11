bace6f05019045272efbb6a1d3242df961031efa
taoql <taoql@gold-finance.com.cn>
2017-07-11 15:23:34 +0800

    by taoqili init

--- meta.js
+++ meta.js
@@ -28,5 +28,5 @@ module.exports = {
   "filters": {
 
   },
-  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}yarn install\n  yarn run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };

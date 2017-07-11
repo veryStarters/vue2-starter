c92f7f5a097ddde435f2f21c9fbdbd6d14c82d4d
taoql <taoql@gold-finance.com.cn>
2017-07-11 15:24:22 +0800

    by taoqili init

--- template/src/router/routes.custom.js
+++ template/src/router/routes.custom.js
@@ -1,7 +1,7 @@
 /**
  * 配置路由中除了name之外的其余参数,
  * key的命名规则：按照pages文件夹下的文件路径来命名,忽略pages目录和index.vue文件 如/pages/user/login/index.vue  文件对应的key即为userLogin
- * @params children 包含所有子路由集合的component对象，命名规则按照『 name ＋　子name 』格式进行
+ * @params children 包含所有子路由集合的component对象，命名规则按照『name＋子name』格式进行
  * @author taoqili
  * @date 2017/4/24
  */

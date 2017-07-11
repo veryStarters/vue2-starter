55b3b88d149d8f2f59ca1e1503675a71c5035c35
taoql <taoql@gold-finance.com.cn>
2017-07-11 17:39:40 +0800

    by taoqili init

--- template/src/main.js
+++ template/src/main.js
@@ -36,7 +36,22 @@ const router = new VueRouter({
     if (savedPosition) {
       return savedPosition
     } else {
-      return {x: 0, y: 0}
+      let position = {}
+      // new navigation.
+      // scroll to anchor by returning the selector
+      if (to.hash) {
+        position.selector = to.hash
+      }
+      // check if any matched route config has meta that requires scrolling to top
+      if (to.matched.some(m => m.meta.scrollToTop)) {
+        // cords will be used if no selector is provided,
+        // or if the selector didn't match any element.
+        position.x = 0
+        position.y = 0
+      }
+      // if the returned position is falsy or an empty object,
+      // will retain current scroll position.
+      return position
     }
   }
 })

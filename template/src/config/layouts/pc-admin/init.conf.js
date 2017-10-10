0b21b9308b7ac35e40ba7ad4049266bebbba0864
taoql <taoql@gold-finance.com.cn>
2017-10-10 17:31:20 +0800

    新增请求和响应切面

--- template/src/config/layouts/pc-admin/init.conf.js
+++ template/src/config/layouts/pc-admin/init.conf.js
@@ -4,7 +4,7 @@ import 'element-ui/lib/theme-default/index.css'
 Vue.use(ElementUI)
 // 此处返回配置项
 export default {
-  topbar: 0,              // 0 hidden; 1 show; 2 fixed
+  topbar: 2,              // 0 hidden; 1 show; 2 fixed
   sidebar: 1,             // 0 hidden; 1 show;
   sidebarTheme: 'dark'    // light; dark
 }

1409b7fe647b4682ce0d9327fde1f7966fdfebfc
taoql <taoql@gold-finance.com.cn>
2017-10-13 13:48:52 +0800

    优化部分文件

--- template/src/config/layout.js
+++ template/src/config/layout.js
@@ -3,7 +3,8 @@
  * @author taoqili
  * @date 2017/9/30
  */
+// 可以导出多个其它布局
 export const mobileLayout = require('./layouts/mobile/layout.vue')
 // 默认的layout
-const layout = require('./layouts/pc-admin/layout.vue')
+import layout from './layouts/pc-admin/layout.vue'
 export default layout

d84960e0baa21d5d348058ca6cecd9f9f2c31804
taoql <taoql@gold-finance.com.cn>
2017-10-13 11:32:58 +0800

    优化部分文件

--- template/src/config/routes.js
+++ template/src/config/routes.js
@@ -2,6 +2,9 @@
  * Created by Webstorm.
  * @author taoqili
  * @date 2017/9/30
+ * 整个框架的路由由系统自动生成，如需自定义配置相关细节，可在本文件内设置，即可覆盖系统默认
+ * 配置规则：
+ * 以pages下的目录（忽略children）驼峰化后作为该路由的名称进行配置；如有子路由，则需在其父路由中添加children字段来进行配置
  */
 export const common = {
   meta: {

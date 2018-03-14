2e0b51d8665b963c0c536c9e425bdb4bdc562cbc
taoqili <taoqili@126.com>
2018-03-14 13:47:01 +0800

    调整文件结构

--- template/src/common/store/modules.js
+++ template/src/common/store/modules.js
@@ -2,6 +2,5 @@
  * 本文件由系统生成，一般情况下无需修改
  **/
 export const common = require('src/store/modules/common')
-export const index = require('src/store/modules/index')
 export const user = require('src/store/modules/user')
-export const testDdd = require('src/store/modules/test/ddd')
+export const index = require('src/store/modules/index')
